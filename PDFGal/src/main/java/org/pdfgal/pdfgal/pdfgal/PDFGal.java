a89ceb250ea4d0712116137cbcd88f27e51d65ad
Alex Pernas <alexpernas@gmail.com>
2014-03-03 10:54:37 +0100

    Definición de primeira API. First API definition.

---
+++ PDFGal/src/main/java/org/pdfgal/pdfgal/pdfgal/PDFGal.java
@@ -0,0 +1,40 @@
+package org.pdfgal.pdfgal.pdfgal;
+
+import java.util.List;
+
+public interface PDFGal {
+	
+	/**
+	 * This method merges PDFs. Parameter inputUris must contain a list of
+	 * Strings which indicates the URIs where PDFs are located, ordered as
+	 * they must be merged. Parameter outputUri indicates the URI where
+	 * resultant PDF must be stored.
+	 * @param inputUris
+	 * @param outputUri
+	 */
+	public void merge(List<String> inputUris, String outputUri);
+	
+	/**
+	 * This method splits PDFs. Parameter inputUri must contain the URI where the
+	 * input PDF is stored.Parameter outputUri indicates the URI where resultant PDF
+	 * must be stored. Parameter pages represents the list of pages, each element
+	 * of this list indicates the first page of each resultant PDF (the first
+	 * page of the first PDF should not be indicated).
+	 * @param inputUri
+	 * @param outputUri
+	 * @param pages
+	 */
+	public void split(String inputUri, String outputUri, List<Integer> pages);
+	
+	/**
+	 * This method password protects a PDF document. Parameter inputUri must
+	 * contain the URI where the input PDF is stored. Parameter outputUri
+	 * indicates the URI where resultant PDF must be stored. Parameter
+	 * password contains the new password of the PDF.
+	 * @param inputUri
+	 * @param outputUri
+	 * @param password
+	 */
+	public void protect(String inputUri, String outputUri, String password);
+
+}
---
+++ PDFGal/src/main/java/org/pdfgal/pdfgal/pdfgal/impl/PDFGalImpl.java
@@ -0,0 +1,27 @@
+package org.pdfgal.pdfgal.pdfgal.impl;
+
+import java.util.List;
+
+import org.pdfgal.pdfgal.pdfgal.PDFGal;
+
+public class PDFGalImpl implements PDFGal {
+
+	@Override
+	public void merge(List<String> inputUris, String outputUri) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void split(String inputUri, String outputUri, List<Integer> pages) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void protect(String inputUri, String outputUri, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
