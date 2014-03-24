1f0ce2a72fbf538ad8800f6320ff06bedadcbed9
Alex <Alex@AlexPCPort>
2014-03-24 23:38:55 +0100

    Creation of Tests structure and protect method. Creación da estrutura das probas e método protect.

---
+++ PDFGal/src/test/java/org/pdfgal/pdfgal/PDFGalTest.java
@@ -0,0 +1,38 @@
+package org.pdfgal.pdfgal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Class for Unit Testing PDFGal library.
+ * @author Alex
+ *
+ */
+public class PDFGalTest {
+	
+	@Test
+	public void merge(){
+		//TODO
+		assertFalse(true);
+	}
+	
+	@Test
+	public void split(){
+		//TODO
+		assertFalse(true);
+	}
+	
+	@Test
+	public void protect(){
+		//TODO
+		assertFalse(true);
+	}
+	
+	@Test
+	public void unProtect(){
+		//TODO
+		assertFalse(true);
+	}
+
+}
