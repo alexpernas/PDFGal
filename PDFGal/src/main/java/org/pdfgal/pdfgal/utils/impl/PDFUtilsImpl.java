0ab7d813fab7c1f8cf3a0170f00544101a3ee6c8
Alex <Alex@AlexPCPort>
2014-07-04 10:13:47 +0200

    Included exception throwing when uri is null at PDFUtilsImpl. Incluído lanzamento de excepción cando a uri é nula en PDFUtilsImpl.

--- PDFGal/src/main/java/org/pdfgal/pdfgal/utils/impl/PDFUtilsImpl.java
+++ PDFGal/src/main/java/org/pdfgal/pdfgal/utils/impl/PDFUtilsImpl.java
@@ -2,6 +2,7 @@ package org.pdfgal.pdfgal.utils.impl;
 
 import java.io.IOException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.pdfgal.pdfgal.utils.PDFUtils;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,9 @@ public class PDFUtilsImpl implements PDFUtils {
 
 	@Override
 	public Integer getPages(final String uri) throws IOException {
+		if (StringUtils.isBlank(uri)) {
+			throw new IOException();
+		}
 		final PDDocument document = PDDocument.load(uri);
 		return document.getNumberOfPages();
 	}
